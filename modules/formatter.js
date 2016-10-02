319143fbbe9cdfd6fb6712082e2e44b1a7e6e0de
Christophe Coenraets <ccoenraets@ccoenraets-ltm1.internal.salesforce.com>
2016-10-01 21:19:23 -0400

    add actions

--- modules/formatter.js
+++ modules/formatter.js
@@ -14,7 +14,24 @@ exports.formatProperties = properties => {
             fields.push({title: "Link", value: "https://login.salesforce.com/" + property.getId(), short:true});
             fields.push({title: "Bedrooms", value: property.get("Beds__c"), short:true});
             fields.push({title: "Price", value: `${numeral(property.get("Price__c")).format('$0,0')}`, short:true});
-            attachments.push({color: color, fields: fields});
+            attachments.push({
+                color: color,
+                fields: fields,
+                actions: [
+                    {
+                        "name": "broker",
+                        "text": "Broker Ingo",
+                        "type": "button",
+                        "value": "broker"
+                    },
+                    {
+                        "name": "schedule",
+                        "text": "Schedule Visit",
+                        "type": "button",
+                        "value": "visit"
+                    },
+                ]
+            });
         });
         return attachments;
     } else {
